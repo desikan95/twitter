87e9ac5c0a3c4ebaf47f6d21f86306ac8401022f
desikan95 <desikan95@gmail.com>
2019-11-25 17:57:39 -0500

    Added searching for tweets and hashtags/mentions

--- lib/twitter.ex
+++ lib/twitter.ex
@@ -51,7 +51,15 @@ defmodule TwitterEngine do
   def getTweets() do
   end
 
-  def storeTweet(user,msg) do
+  def storeTweet(user,msg,retweet_ctr \\ 0) do
+
+    hashtags =  Regex.scan(~r/#(\w*)/, msg)
+                |> Enum.map(fn(c) -> Enum.at(c,1) end)
+
+
+    mentions =  Regex.scan(~r/@(\w*)/, msg)
+                |> Enum.map( fn(c) -> Enum.at(c,1) end)
+
 
     #Getalluserfollowers
     user_followers = :ets.match_object(:userfollowing, {:'$1',:'$2'})
@@ -94,15 +102,92 @@ defmodule TwitterEngine do
 
        current_time = :calendar.local_time()
        Enum.each(followers,fn(f)->
-         :ets.insert(:users, {f,[msg,current_time,user]})
+         :ets.insert(:users, {f,[msg,hashtags,mentions,current_time,retweet_ctr,user]})
        end)
 
 
-    #Add tweets to user and tweet mappings
-  #  current_time = :calendar.local_time()
-  #  :ets.insert(:users, {user,current_time,msg})
+  end
+
+  #this search is public. Can search tweets even if I'm not subscribed to it
+  def searchTweetsByHashtag(hashtag) do
+    result = :ets.match_object(:users, {:'$1',:'$2'})
+    hashtag_tweets = Enum.map(result, fn (r)->
+                        {_,tweet} = r
+                        hashtags_list = Enum.at(tweet, 1)  #Gets the hashtag list for each result in the table
+                        if (Enum.member?(hashtags_list,hashtag) == true)
+                        do
+                          Enum.at(tweet, 0)  #Return the tweet message, which is stored at 0
+                        end
+                     end)
+                     |> Enum.uniq
+                     |> Enum.reject(fn(x) -> x==:nil end)
 
 
+      IO.puts "List of tweets contains that hashtag are : "
+      IO.inspect hashtag_tweets
+  end
+
+  #this search is also public
+  def getMyMentions(username) do
+    result = :ets.match_object(:users, {:'$1',:'$2'})
+    my_mentions = Enum.map(result, fn (r)->
+                    {_,tweet} = r
+                    mentions_list = Enum.at(tweet, 2)   #Gets the mentions list for each result in the table
+                    if (Enum.member?(mentions_list,username) == true)
+                    do
+                      Enum.at(tweet, 0)   #Return the tweet message, which is stored at 0
+                    end
+                 end)
+                 |> Enum.uniq
+                 |> Enum.reject(fn(x) -> x==:nil end)
+
+      IO.puts "List of my mentions are : "
+      IO.inspect my_mentions
+  end
+
+  #private search. Only querying my subscriber's tweets
+  def searchTweetsSubscribedTo(username,search) do
+    tweets = :ets.lookup(:users,username)
+    tweet_msg = Enum.map(tweets, fn (t)->
+                  {_,tweet_result} = t
+                  Enum.at(tweet_result,0)
+                end)
+
+    IO.puts " Here are the list of messages relevant : "
+    IO.inspect tweet_msg
+
+    {:ok,regex_string} = Regex.compile(search)
+    search_result = Enum.map(tweet_msg,fn (tweet)->
+
+
+                      if (Regex.match?(regex_string,tweet) == true)
+                        do
+                        {
+                          tweet
+                        }
+                      end
+                    end)
+                    |> Enum.reject (fn x -> x==:nil end)
+    IO.puts "Here are the list of valid searches"
+    IO.inspect search_result
+  end
+
+  #public. Can retweet a random message
+  def retweets(username) do
+    result = :ets.match_object(:users, {:'$1',:'$2'})
+
+    random_result = Enum.map(result, fn (r)->
+                      {_,result} = r
+                      result
+                    end)
+                    |> Enum.random
+    tweet_msg = Enum.at(random_result,0)
+    retweet_ctr = Enum.at(random_result,4)
+
+    IO.puts "Retweeting the following message : "
+    IO.inspect tweet_msg
+
+    TwitterEngine.storeTweet(username, tweet_msg, retweet_ctr+1)
   end
 
   def addFollowing(user1,user2) do
@@ -123,7 +208,7 @@ defmodule TwitterEngine do
     #random following
   end
 
-  def showmessages(user) do
+  def getTweets(user) do
     value = :ets.lookup(:users,user)
     IO.puts "Messages of this user are "
     IO.inspect value
