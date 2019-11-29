a957914eca09b3a1b47ec65cacc269304c4e834b
desikan95 <desikan95@gmail.com>
2019-11-29 18:27:25 -0500

    Added report and updated proj4.exs

--- proj4.exs
+++ proj4.exs
@@ -4,18 +4,15 @@ defmodule TwitterApp do
   def start(_type,_args) do
 
     arguments = System.argv()
-    {numUsers,_}=Integer.parse(Enum.at(arguments,0))
-    {numMessages,_}=Integer.parse(Enum.at(arguments,1))
-
-    IO.puts "Users "
-    IO.inspect numUsers
-    IO.puts "Number of messages"
-    IO.inspect numMessages
 
     pid = TwitterEngine.start_link(1)
-    ClientSupervisor.simulate(numUsers, numMessages)
-
-    IO.puts "Started application"
+    if (Enum.at(arguments,0)!="test")
+    do
+        {numUsers,_}=Integer.parse(Enum.at(arguments,0))
+        {numMessages,_}=Integer.parse(Enum.at(arguments,1))
+        ClientSupervisor.simulate(numUsers,numMessages)
+        IO.puts " Done with simulation !"
+    end
 
     {:ok,pid}
   end
